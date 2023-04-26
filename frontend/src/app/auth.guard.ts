23e8a269988617161cb1ae168839da33bcd09615
ARULSARAVANAN1 <saravansnp@gmail.com>
2023-04-26 23:24:21 +0530

    First commit

---
+++ frontend/src/app/auth.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs";
+import { LoginService } from "./login.service";
+
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+export class AuthGuard implements CanActivate{
+  
+    status: boolean = false;
+    constructor(private loginServices: LoginService, private route :Router) {}
+  
+    canActivate(
+      route: ActivatedRouteSnapshot, 
+      router: RouterStateSnapshot
+    ): 
+
+    boolean {
+      if(this.isloggedIn()){
+        return true;
+      }
+      this.route.navigate(['login']);
+      return false;
+    }
+  
+    public isloggedIn(){
+      let status = false;
+      if(this.loginServices.loginstatus===true){
+        status=true;
+      }
+      else{
+        status=false;
+      }
+      return status;
+    }
+  
+  }
